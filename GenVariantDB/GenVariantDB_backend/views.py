32372cf6b10d95a3d58467db0f4333d4da917d4c
YssfTarek <ytabdou@gmail.com>
2024-03-20 09:51:16 +0200

    commented our render from views

--- GenVariantDB/GenVariantDB_backend/views.py
+++ GenVariantDB/GenVariantDB_backend/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
 import os
 from dotenv import load_dotenv
