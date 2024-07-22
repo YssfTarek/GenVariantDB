3f94e9390e3bb686238b2fc7a0326d89e84429ad
YssfTarek <ytabdou@gmail.com>
2024-07-22 10:24:55 +0300

    Completed admin page and aggregations

--- GenVariantDB/GenVariantDB_backend/urls.py
+++ GenVariantDB/GenVariantDB_backend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import connect, writeManifest
+from .views import connect, writeManifest, getDocCount, get_var_stats
 
 urlpatterns = [
     path("connect/", connect, name="connect"),
     path("writeManifest/", writeManifest, name="writeManifest"),
+    path("getDocCount/", getDocCount, name="getDocCount"),
+    path ("get_var_stats/", get_var_stats, name="get_var_stats")
 ]
--- GenVariantDB/GenVariantDB_backend/views.py
+++ GenVariantDB/GenVariantDB_backend/views.py
@@ -1,6 +1,6 @@
 from django.views.decorators.csrf import csrf_exempt
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import os
 from dotenv import load_dotenv
 from .utils import MongoConnect
@@ -17,6 +17,170 @@ DB = os.getenv("DB")
 collection = os.getenv("Collection")
 
 # Create your views here.
+
+def get_var_stats(request):
+    db_handle = MongoConnect(DB_URL, DB, collection)
+
+    pipeline = [
+        {
+            '$project': {
+                'synonymous': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*synonymous.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }, 
+                'missense': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*missense.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }, 
+                'nonsense': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*nonsense.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }, 
+                'insertion': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*insertion.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }, 
+                'deletion': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*deletion.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }, 
+                'silent': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*silent.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }, 
+                'frameshift': {
+                    '$size': {
+                        '$filter': {
+                            'input': '$variants.INFO', 
+                            'as': 'info', 
+                            'cond': {
+                                '$regexMatch': {
+                                    'input': '$$info.FC', 
+                                    'regex': '.*frameshift.*', 
+                                    'options': 'i'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }, {
+            '$group': {
+                '_id': None, 
+                'totalSynonymous': {
+                    '$sum': '$synonymous'
+                }, 
+                'totalMissense': {
+                    '$sum': '$missense'
+                }, 
+                'totalNonsense': {
+                    '$sum': '$nonsense'
+                }, 
+                'totalInsertion': {
+                    '$sum': '$insertion'
+                }, 
+                'totalDeletion': {
+                    '$sum': '$deletion'
+                }, 
+                'totalSilent': {
+                    '$sum': '$silent'
+                }, 
+                'totalFrameshift': {
+                    '$sum': '$frameshift'
+                }
+            }
+        }
+    ]
+    
+    if request.method == "GET":
+        var_stats = list(db_handle.aggregate(pipeline))
+        response = JsonResponse(var_stats, status = 200, safe=False)
+        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        response["Access-Control-Allow-Headers"] = "Content-Type"
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        return response
+    else:
+        return HttpResponse("Invalid request", status = 400)
+
+
+def getDocCount(request):
+    db_handle = MongoConnect(DB_URL, DB, collection)
+    
+    if request.method == "GET":
+        doc_count = db_handle.count_documents({})
+        response = JsonResponse({"total_documents": doc_count}, status=200)
+        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        response["Access-Control-Allow-Headers"] = "Content-Type"
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        return response
+    else:
+        return HttpResponse("Invalid request", status=400)
 
 @csrf_exempt
 def writeManifest(request):
